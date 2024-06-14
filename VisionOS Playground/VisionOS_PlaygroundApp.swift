44764ad67243f2c258f4d235d84b081d6e136df9
Aaron <dev@interactivecode.co.uk>
2024-06-14 08:27:14 +0100

    Initial Commit

---
+++ VisionOS Playground/VisionOS_PlaygroundApp.swift
@@ -0,0 +1,18 @@
+//
+//  VisionOS_PlaygroundApp.swift
+//  VisionOS Playground
+//
+//  Created by Aaron Thomas on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct VisionOS_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
---
+++ VisionOS PlaygroundTests/VisionOS_PlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS_PlaygroundTests.swift
+//  VisionOS PlaygroundTests
+//
+//  Created by Aaron Thomas on 14/06/2024.
+//
+
+import Testing
+@testable import VisionOS_Playground
+
+struct VisionOS_PlaygroundTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
